61d717f973b3fb6976238667453d64dd7410111d
Julia <jgolin129@gmail.com>
2019-12-18 17:17:49 -0500

    hover feature

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ d3.dsv(',','employmentbyindustry.csv',function(d) {
         console.log(counties);
 
         // load map and position
-        var center = [41.5, -72.8];
-        var map = L.map('mapid').setView(center, 8.5);
+        let center = [41.5, -72.8];
+        let map = L.map('mapid').setView(center, 8.5);
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
@@ -39,11 +39,63 @@ d3.dsv(',','employmentbyindustry.csv',function(d) {
                 opacity: 1,
                 color: 'white',
                 dashArray: '3',
-                fillOpacity: 0.75
+                fillOpacity: 0.5
             };
         }
 
         // add data from geojson to map
         L.geoJSON(counties, {style: style}).addTo(map);
+
+        // highlight county when mouseover
+        function highlightCounty(e) {
+            let layer = e.target;
+
+            layer.setStyle({
+                weight: 5,
+                color: '#333333',
+                dashArray: ''
+            });
+
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+            }
+        }
+
+        let geojson;
+        geojson = L.geoJSON(counties);
+
+        function resetCounty(e) {
+            geojson.resetStyle(e.target);
+        }
+
+        function onEachFeature(feature, layer) {
+            layer.on({
+                mouseover: highlightCounty,
+                mouseout: resetCounty
+            });
+        }
+
+        geojson = L.geoJSON(counties, {
+            style: style,
+            onEachFeature: onEachFeature
+        }).addTo(map);
+
+        // control that shows county info on hover
+        var info = L.control();
+
+        info.onAdd = function (map) {
+            this._div = L.DomUtil.create('div', 'info');
+            this.update();
+            return this._div;
+        };
+
+        info.update = function (props) {
+            this._div.innerHTML = '<h4>CT County Population</h4>' +  (props ?
+                '<b>' + props.name + '</b><br />' + props.population + ' people / mi<sup>2</sup>'
+                : 'Hover over a county');
+        };
+
+        info.addTo(map);
+
     });
 });
